4f2a0a00b0728cce4cd4fa0506e1a743be2cecbe
Indhu201 <indhum2002@gmail.com>
2024-03-25 11:35:59 +0530

    Add Dummyjson get product and get product by id

--- handlers/products.js
+++ handlers/products.js
@@ -1,4 +1,38 @@
+                                                                                                                                       
+import axios from 'axios';
+
+let products = [];
+
+axios.get('https://dummyjson.com/products?limit=10')
+  .then(response => {
+    products = response.data;
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
 
 export const demoHandler = (req, res) => {
   res.send("Demo Handler")
 }
+
+export const getProducts = (req, res) => {
+  res.json(products);
+};
+
+// GET EACH PRODUCT 
+export const getProductById = (req, res) => {
+  fetch('https://dummyjson.com/products/1')
+  .then(res => res.json())
+  .then(product => {
+    res.json(product); // Send the fetched product back as a response
+  })
+  .catch(error => {
+    console.error('Error fetching product:', error);
+    res.status(500).json({ error: 'Failed to fetch product' });
+  });
+  }
+
+
+
+
+
